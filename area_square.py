25059d0e771b8f4737112661fd56fb38e0c8f9ad
Carlos Abreu <carlosy@Winmuy.localdomain>
2022-05-13 19:20:19 +0100

    add calculate_rhombus_are function

--- area_square.py
+++ area_square.py
@@ -9,3 +9,9 @@ def build_ferrari(color: str = 'red') -> str:
     print(f"Built a {color} Ferrari")
 
 build_ferrari('blue')
+
+# Prompt the user to select a shape and wait
+selection = input("""\t'C' - Circle
+\t'S' - Square
+\t'R' - Rectangle
+""")
