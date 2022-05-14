09b8d4137696ca901b560dc56122db83a4de53d8
Carlos Abreu <carlos.abreu@zoho.com>
2022-05-14 20:20:25 +0100

    calculate_area file included square, rectangle and cicle

--- calculate_area.py
+++ calculate_area.py
@@ -1,11 +1,39 @@
 
+
+def calculate_square_area(side: float) -> float:
+    return side * side
+
+def calculate_rectangle_area(length: float, width: float) -> float:
+    return length * width
+
+def calculate_circle_area(radius: float) -> float:
+    return math.pi * radius ** 2
+
+print("""
+   --------------------------------------
+   Area Calculator
+   -------------------------------------- 
+   Select a shape:
+""")
+
+selection = input("""\t'S' - Square
+\t'R' - Rectangle
+\t'C' - Circle
+""")
+
 area = 0
 
-def calculate_square_area(side: int) -> int:
-    global area
-    area = side * side
-    print(f"The area is {area}")
+if selection == 'S':
+    side = input("Enter the side: ")
+    area = calculate_square_area(float(side))
+elif selection == 'R':
+    lenght = input("Enter the length: ")
+    width = input("Enter the width: ")
+    area = calculate_rectangle_area(float(lenght), float(width))
+elif selection == 'C':
+    radius = input("Enter the radious: ")
+    area = calculate_circle_area(float(radius))
+else:
+    print("Invalid selection. Choose 'S', 'R', 'C'")
 
-calculate_square_area(4)
-
-print(f"Write out the value of area {area}")
+print(f"Write out the area: {area}")
