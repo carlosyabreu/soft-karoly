c7adcd7a81053c00eace6ef087017f2c365e5b7d
Carlos Abreu <carlos.abreu@zoho.com>
2022-05-15 21:02:45 +0100

    Add is_authentication check

--- login.py
+++ login.py
@@ -10,9 +10,20 @@ def login(user: str, passw: str) -> bool:
 username: str = input('Username: ')
 password: str = input('Password: ')
 
+attempt = 0
+max_attempts = 3
+is_authentication = False
+
 while login(username, password) == False:
+    attempt += 1
+    if attempt > max_attempts: break
+
     print('Login failed, re-enter your credentials: ')
     username: str = input('Username: ')
     password: str = input('Password: ')
+else:
+    is_authentication = True
+    print('Login successful')
 
-print('Login successful')
+if not is_authentication:
+    print('Your login has been temporarily locked.')
