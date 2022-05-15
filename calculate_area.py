861ea97498db4b1b842671a5cd2fa10da2d321f1
Carlos Abreu <carlos.abreu@zoho.com>
2022-05-15 20:14:33 +0100

    Update calculate_area function adding 'B' value on print statement

--- calculate_area.py
+++ calculate_area.py
@@ -10,6 +10,9 @@ def calculate_circle_area(radius: float) -> float:
     pi : int = 3.14
     return pi * radius ** 2
 
+def calculate_rhombus_area(p: int, q: int):
+    return p * q / 2
+
 print("""
    --------------------------------------
    Area Calculator
@@ -20,6 +23,7 @@ print("""
 selection = input("""\t'S' - Square
 \t'R' - Rectangle
 \t'C' - Circle
+\t'B' - Rhombus
 """)
 
 
@@ -28,7 +32,6 @@ def calculate_area(selection: str):
 
     if selection == 'S':
         side = input("Enter the side: ")
-        area = calculate_square_area(float(side))
     elif selection == 'R':
         lenght = input("Enter the length: ")
         width = input("Enter the width: ")
@@ -36,8 +39,12 @@ def calculate_area(selection: str):
     elif selection == 'C':
         radius = input("Enter the radious: ")
         area = calculate_circle_area(float(radius))
+    elif selection == 'B':
+        p = input('Enter the p value: ')
+        q = input('Enter the q value: ')
+        area = calculate_rhombus_area(float(p), float(q))
     else:
-        print("Invalid selection. Choose 'S', 'R', 'C'")
+        print("Invalid selection. Choose 'S', 'R', 'C', 'B'")
 
     return area
 
@@ -49,7 +56,8 @@ def getShapeArea(tag: str):
         shape = 'Rectangle'
     elif tag == 'C':
         shape = 'Circle'
-    
+    elif tag == 'B':
+        shape = 'Rhombus'
     return shape
 
 area = calculate_area(selection)
