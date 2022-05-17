554b06a5cac56c4193341ea75a19038aa766b718
Carlos Abreu <carlos.abreu@zoho.com>
2022-05-17 20:09:50 +0100

    Not print zero (0)

--- for-loop.py
+++ for-loop.py
@@ -1,5 +1,8 @@
 
 for index in range(5):
     # index works within the loop
-    if index % 2 == 0: 
+#    if index % 2 == 0: 
+#        print(f"{index} - iteration count {index + 1}")
+    if index == 0: continue
+    elif index % 2 == 0:
         print(f"{index} - iteration count {index + 1}")
