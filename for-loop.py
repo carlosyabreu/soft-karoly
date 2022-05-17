cae95245af3d8a28f552fa600ab9cc8a0f6270f5
Carlos Abreu <carlos.abreu@zoho.com>
2022-05-17 20:05:10 +0100

    Update for-loop file

--- for-loop.py
+++ for-loop.py
@@ -1,5 +1,5 @@
 
 for index in range(5):
     # index works within the loop
-    if index == 2: break
-    print(f"{index} - iteration count {index + 1}")
+    if index % 2 == 0: 
+        print(f"{index} - iteration count {index + 1}")
