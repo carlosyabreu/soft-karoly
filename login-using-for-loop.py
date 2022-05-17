6997fd28e573296df29c495bbe0aec40d0a787cb
Carlos Abreu <carlos.abreu@zoho.com>
2022-05-17 20:25:19 +0100

    login-using-for-loop file finalised

--- login-using-for-loop.py
+++ login-using-for-loop.py
@@ -15,6 +15,9 @@ is_authentication = False
 for attempt in range(4):
     if login(username, password) == True:
         is_authentication = True
+        print("Login successful")
         break
     else:
         print("Login failed, re-enter your credentials.")
+        username: str = input('Username: ')
+        password: str = input('Password: ')
