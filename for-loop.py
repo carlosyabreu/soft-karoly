7590952b9ff75b0d4066a87f8747716335866096
Carlos Abreu <carlos.abreu@zoho.com>
2022-05-19 23:51:34 +0100

    Retest again

--- for-loop.py
+++ for-loop.py
@@ -3,6 +3,8 @@ for index in range(5):
     # index works within the loop
 #    if index % 2 == 0: 
 #        print(f"{index} - iteration count {index + 1}")
+    # Teste
     if index == 0: continue
     elif index % 2 == 0:
-        print(f"{index} - iteration count {index + 1}")
+        print(f"{index} - iteration count {index + 1})"
+        
