cb2f436157c32e607686b5ce38f11bcb512f6c06
Carlos Abreu <carlos.abreu@zoho.com>
2022-05-21 16:43:35 +0100

    Change list file adding a different list

--- lists.py
+++ lists.py
@@ -2,14 +2,24 @@
 # primes = list()
 # print(primes)
 
-primes = []
-primes.append(2)
-primes.append(3)
-primes.append(5)
-primes.append(7)
+# primes = []
+# primes.append(2)
+# primes.append(3)
+# primes.append(5)
+# primes.append(7)
 
-print(primes)
+# print(primes)
 
-list = ["Michael", "Peter", "Carlos"]
-name = list[2]
-print(name)
+list = ["Michael", "Peter", "Carlos", "Joanna"]
+# name = list[2]
+# print(name)
+
+def is_valid_index(index: int, list: list) -> bool:
+    result = False
+
+    if 0 <= index and index < len(list):
+        result = True
+    return result
+
+index = 3
+print(f"Index {index} is valid" if is_valid_index(index, list) else f"Index {index} is out of range")
